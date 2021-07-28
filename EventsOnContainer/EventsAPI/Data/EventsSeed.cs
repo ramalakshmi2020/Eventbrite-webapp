2afde8dd7e950ad063c842dacf47f836ea990a44
stackedwaffles88 <tripcmom@gmail.com>
2021-07-28 15:01:46 -0400

    Added Roxanne's Events

--- EventsOnContainer/EventsAPI/Data/EventsSeed.cs
+++ EventsOnContainer/EventsAPI/Data/EventsSeed.cs
@@ -73,7 +73,42 @@ namespace EventsAPI.Data
                     Lon = -122.2006M,
                     City = "Bellevue",
                     State = "WA"
-                }
+                },
+                new Location
+                {
+                    Lat = 39.2974M,
+                    Lon = -78.4342M,
+                    City = "Capon Bridge",
+                    State = "WV"
+                },
+                new Location
+                {
+                    Lat = 37.7775M,
+                    Lon = -81.1748M,
+                    City = "Beckley",
+                    State = "WV"
+                },
+                new Location
+                {
+                    Lat = 39.3321M,
+                    Lon = -77.9048M,
+                    City = "Kearneysville",
+                    State = "WV"
+                },
+                new Location
+                {
+                    Lat = 38.3510M,
+                    Lon = -81.6278M,
+                    City = "Charleston",
+                    State = "WV"
+                },
+                new Location
+                {
+                    Lat = 37.3696M,
+                    Lon = -81.0958M,
+                    City = "Princeton",
+                    State = "WV"
+                },
             };
         }
 
@@ -86,10 +121,11 @@ namespace EventsAPI.Data
                     EventsTypeId = 3,
                     EventCreaterId = 1,
                     LocationId = 1,
-                    Description="Seattle Tech Career Fair:Exclusive Tech Hiring Event-New",
-                    Name="Tech Career Fair",
-                    Price=50.9M,PictureUrl="http://externaleventsbaseurltobereplaced/api/pic/1",
-                    StartTime=new DateTime(2021,08,15,5,10,20,DateTimeKind.Local),
+                    Description = "Seattle Tech Career Fair:Exclusive Tech Hiring Event-New",
+                    Name = "Tech Career Fair",
+                    Price = 50.9M,
+                    PictureUrl = "http://externaleventsbaseurltobereplaced/api/pic/1",
+                    StartTime = new DateTime(2021,08,15,5,10,20,DateTimeKind.Local),
                     EndTime = new DateTime(2021,08,15,6,10,20,DateTimeKind.Local),
                 },
                 new Event
@@ -97,10 +133,10 @@ namespace EventsAPI.Data
                     EventsTypeId = 3,
                     EventCreaterId = 2,
                     LocationId = 1,
-                    Description="Develop a Successful GreenTech / CleanTech Startup Business Today",
-                    Name="GreenTech / CleanTech",
-                    Price=50.9M,PictureUrl="http://externaleventsbaseurltobereplaced/api/pic/2",
-                    StartTime=new DateTime(2021,08,16,5,10,20,DateTimeKind.Local),
+                    Description = "Develop a Successful GreenTech / CleanTech Startup Business Today",
+                    Name = "GreenTech / CleanTech",
+                    Price = 50.9M,PictureUrl="http://externaleventsbaseurltobereplaced/api/pic/2",
+                    StartTime = new DateTime(2021,08,16,5,10,20,DateTimeKind.Local),
                     EndTime = new DateTime(2021,08,16,6,10,20,DateTimeKind.Local),
                 },
                 new Event
@@ -108,10 +144,11 @@ namespace EventsAPI.Data
                     EventsTypeId = 3,
                     EventCreaterId = 3,
                     LocationId = 3,
-                    Description="Thinkful Webinar | Getting Started in Tech",
-                    Name="Getting Started in Tech",
-                    Price=50.9M,PictureUrl="http://externaleventsbaseurltobereplaced/api/pic/3",
-                    StartTime=new DateTime(2021,08,17,5,10,20,DateTimeKind.Local),
+                    Description = "Thinkful Webinar | Getting Started in Tech",
+                    Name = "Getting Started in Tech",
+                    Price=50.9M,
+                    PictureUrl = "http://externaleventsbaseurltobereplaced/api/pic/3",
+                    StartTime = new DateTime(2021,08,17,5,10,20,DateTimeKind.Local),
                     EndTime = new DateTime(2021,08,17,6,10,20,DateTimeKind.Local),
                 },
                 new Event
@@ -119,10 +156,11 @@ namespace EventsAPI.Data
                     EventsTypeId = 3,
                     EventCreaterId = 2,
                     LocationId = 4,
-                    Description="Develop a Successful Healthcare Tech Startup Business Hackathon | Healthcare Tech Entrepreneurship Workshop | Medtech Entrepreneur Training",
-                    Name="Healthcare Tech Startup Business",
-                    Price=50.9M,PictureUrl="http://externaleventsbaseurltobereplaced/api/pic/4",
-                    StartTime=new DateTime(2021,08,18,5,10,20,DateTimeKind.Local),
+                    Description = "Develop a Successful Healthcare Tech Startup Business Hackathon | Healthcare Tech Entrepreneurship Workshop | Medtech Entrepreneur Training",
+                    Name = "Healthcare Tech Startup Business",
+                    Price = 50.9M,
+                    PictureUrl = "http://externaleventsbaseurltobereplaced/api/pic/4",
+                    StartTime = new DateTime(2021,08,18,5,10,20,DateTimeKind.Local),
                     EndTime = new DateTime(2021,08,18,6,10,20,DateTimeKind.Local),
                 },
                 new Event
@@ -130,12 +168,72 @@ namespace EventsAPI.Data
                     EventsTypeId = 3,
                     EventCreaterId = 2,
                     LocationId = 3,
-                    Description="Develop a Successful Food Tech Startup Business Hackathon | Startup Hackathon | Entrepreneur Hackathon | Entrepreneurship Hackathon",
-                    Name="Food Tech Startup Business",
-                    Price=50.9M,PictureUrl="http://externaleventsbaseurltobereplaced/api/pic/5",
-                    StartTime=new DateTime(2021,08,19,5,10,20,DateTimeKind.Local),
+                    Description = "Develop a Successful Food Tech Startup Business Hackathon | Startup Hackathon | Entrepreneur Hackathon | Entrepreneurship Hackathon",
+                    Name = "Food Tech Startup Business",
+                    Price = 50.9M,PictureUrl="http://externaleventsbaseurltobereplaced/api/pic/5",
+                    StartTime = new DateTime(2021,08,19,5,10,20,DateTimeKind.Local),
                     EndTime = new DateTime(2021,08,19,6,10,20,DateTimeKind.Local),
-                }
+                },
+                new Event
+                {
+                    EventsTypeId = 4,
+                    EventCreaterId = 6,
+                    LocationId = 6,
+                    Description = "Join us at The River House in Capon Bridge, WV every Saturday from 12-3 for a free drop in art activity. All materials provided!",
+                    Name = "Art for All- Free Art Activity for all ages!",
+                    Price = 0.0M,
+                    PictureUrl = "",
+                    StartTime = new DateTime(2021,07,31,12,00,20,DateTimeKind.Local),
+                    EndTime = new DateTime(2021,07,31,3,00,20,DateTimeKind.Local),
+                },
+                new Event
+                {
+                    EventsTypeId = 4,
+                    EventCreaterId = 7,
+                    LocationId = 7,
+                    Description = "The Ecstasy of Gold is Beckley Art Center's newest solo exhibition featuring the collage artistry of artist, Psychoflauge.",
+                    Name = "The Ecstasy of Gold | Exhibition Opening Reception",
+                    Price = 0.0M,
+                    PictureUrl = "",
+                    StartTime = new DateTime(2021,08,6,6,00,20,DateTimeKind.Local),
+                    EndTime = new DateTime(2021,08,6,7,30,20,DateTimeKind.Local),
+                },
+                new Event
+                {
+                    EventsTypeId = 4,
+                    EventCreaterId = 8,
+                    LocationId = 8,
+                    Description = "Join your friends at a top judged festival in America! 45th Annual Fall Mountain Heritage Arts & Crafts Festival 2021, FREE WV Wine Tastings",
+                    Name = "45th Annual Fall Mountain Heritage Arts & Crafts Festival",
+                    Price = 0.0M,
+                    PictureUrl = "",
+                    StartTime = new DateTime(2021,09,24,10,00,20,DateTimeKind.Local),
+                    EndTime = new DateTime(2021,09,26,5,00,20,DateTimeKind.Local),
+                },
+                new Event
+                {
+                    EventsTypeId = 4,
+                    EventCreaterId = 9,
+                    LocationId = 9,
+                    Description = "FIGURE DRAWING @ Studio 3D July - August Session Starting July 29th we're hosting 4 Figure Drawing sessions!",
+                    Name = "Figure Drawing July-August Session",
+                    Price = 40.0M,
+                    PictureUrl = "",
+                    StartTime = new DateTime(2021,07,27,6,00,20,DateTimeKind.Local),
+                    EndTime = new DateTime(2021,08,27,9,00,20,DateTimeKind.Local),
+                },
+                new Event
+                {
+                    EventsTypeId = 4,
+                    EventCreaterId = 10,
+                    LocationId = 10,
+                    Description = "We will be painting a Sunflower in the style of Van Gogh. All materials are included to paint your masterpiece. No experience necessary, our",
+                    Name = "Peak Of The Bloom Sunflower Painting Class",
+                    Price = 29.99M,
+                    PictureUrl = "",
+                    StartTime = new DateTime(2021,08,14,5,00,20,DateTimeKind.Local),
+                    EndTime = new DateTime(2021,08,14,8,00,20,DateTimeKind.Local),
+                },
 
             };
         }
@@ -190,6 +288,26 @@ namespace EventsAPI.Data
                 new EventsHost
                 {
                     Creater = " AnalyticsWEEK"
+                },
+                new EventsHost
+                {
+                    Creater = "The River House"
+                },
+                new EventsHost
+                {
+                    Creater = "Beckley Art Center"
+                },
+                new EventsHost
+                {
+                    Creater = "Jefferson County Chamber of Commerce, Inc."
+                },
+                new EventsHost
+                {
+                    Creater = "Tamarack Foundation for the Arts"
+                },
+                new EventsHost
+                {
+                    Creater = "The Bronze Look, LLC"
                 }
             };
         }
