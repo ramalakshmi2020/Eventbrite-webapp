f588152d712fd324a458f5fb1a9e7b0ad7f9a9be
ramalakshmi2020 <ramalakshmi.n@gmail.com>
2021-07-27 23:08:20 -0700

    Update EventsSeed.cs
    Added few more seed data for events

--- EventsOnContainer/EventsAPI/Data/EventsSeed.cs
+++ EventsOnContainer/EventsAPI/Data/EventsSeed.cs
@@ -135,6 +135,67 @@ namespace EventsAPI.Data
                     Price=50.9M,PictureUrl="http://externaleventsbaseurltobereplaced/api/pic/5",
                     StartTime=new DateTime(2021,08,19,5,10,20,DateTimeKind.Local),
                     EndTime = new DateTime(2021,08,19,6,10,20,DateTimeKind.Local),
+                },
+                               
+                new Event
+                {
+                    Description = "Cooking Class ~ Homemade Tomato Risotto with Burrata and Basil ",
+                    Name = " Tomato Risotto with Burrata and Basil",
+                    Price = 5,
+                    StartTime = new DateTime(2021, 9, 18, 16, 0, 0, DateTimeKind.Local),
+                    EndTime = new DateTime(2021, 9, 18, 16, 0, 0, DateTimeKind.Local),
+                    EventCreaterId = 6,
+                    EventsTypeId = 5,
+                    LocationId = 1,
+                    PictureUrl = ""
+                },
+                new Event
+                {
+                    Description = "Cooking Class ~ Jambalaya with Saffron Aioli! Presented with our Buddies at SCANPAN & GLOBAL Cutlery",
+                    Name = "Jambalaya with Saffron Aioli",
+                    Price = 3,
+                    StartTime = new DateTime(2021, 9, 9, 16, 0, 0, DateTimeKind.Local),
+                    EndTime = new DateTime(2021, 9, 9, 16, 0, 0, DateTimeKind.Local),
+                    EventCreaterId = 6,
+                    EventsTypeId = 5,
+                    LocationId = 1,
+                    PictureUrl = ""
+                },
+                new Event
+                {
+                    Description = "Cooking Class ~ Vegan Pie with Sudha",
+                    Name = "Vegan Pie",
+                    Price = 5,
+                    StartTime = new DateTime(2021, 9, 15, 16, 0, 0, DateTimeKind.Local),
+                    EndTime = new DateTime(2021, 9, 15, 16, 0, 0, DateTimeKind.Local),
+                    EventCreaterId = 6,
+                    EventsTypeId = 5,
+                    LocationId = 1,
+                    PictureUrl = ""
+                },
+                new Event
+                {
+                    Description =" More and more people are interested in vegan/plant-based eating. Some are just curious, some want to get their feet wet, and some are ready to come to the V-side - ",
+                    Name = "Vegan For Beginners",
+                    Price = 3,
+                    StartTime = new DateTime(2021, 9, 18, 17,0,0,DateTimeKind.Local),
+                    EndTime = new DateTime(2021, 9, 18, 18,0,0,DateTimeKind.Local),
+                    EventCreaterId = 7,
+                    EventsTypeId = 5,
+                    LocationId = 1,
+                    PictureUrl = ""
+                },
+                new Event
+                {
+                    Description ="Homemade Free Cooking Class ~ Cheese Soufflé and Summer Salad with Green Goddess Dressing, with our Buddies at Cabot Cheese ",
+                    Name = "Cheese Soufflé and Summer Salad",
+                    Price = 0,
+                    StartTime = new DateTime(2021, 9, 28, 16,0,0,DateTimeKind.Local),
+                    EndTime = new DateTime(2021, 9, 28, 17,0,0,DateTimeKind.Local),
+                    EventCreaterId = 6,
+                    EventsTypeId = 5,
+                    LocationId = 1,
+                    PictureUrl = ""
                 }
 
             };
@@ -190,6 +251,14 @@ namespace EventsAPI.Data
                 new EventsHost
                 {
                     Creater = " AnalyticsWEEK"
+                },
+                new EventsHost
+                {
+                    Creater = "Nola Ro"
+                },
+                new EventsHost
+                {
+                    Creater = "Homemade Events"
                 }
             };
         }
