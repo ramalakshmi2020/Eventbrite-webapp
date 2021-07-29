031d91ad883283c6ad348f2549e0db9c81b8ea15
Asha Hakari <asha.hakari@gmail.com>
2021-07-29 09:27:27 -0700

    adding PicController
    adding first controller and first api

---
+++ EventsOnContainer/EventsAPI/Controllers/PicController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PicController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+        public PicController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+        [Route("{id}")]
+        public IActionResult GetImage(int id)
+        {
+            var webroot = _env.WebRootPath;
+            var path = Path.Combine($"{webroot}/pics/",$"event{id}.jfif");
+            var buffer = System.IO.File.ReadAllBytes(path);
+
+            return File(buffer, "image/jpeg");
+        }
+    }
+}
